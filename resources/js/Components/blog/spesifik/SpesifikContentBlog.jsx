8be3cee3fda97d7a29b47dea22bde63dbdbfd468
yoga <yogaagle123z@gmail.com>
2023-08-06 03:17:02 +0700

    blog clear

--- resources/js/Components/blog/spesifik/SpesifikContentBlog.jsx
+++ resources/js/Components/blog/spesifik/SpesifikContentBlog.jsx
@@ -153,7 +153,7 @@ export default function SpesifikContentBlog({ dataContent: item }) {
                 <div className="contack  pt-20 px-5">
                     <div className="bg-white relative w-[20rem] lg:w-full justify-center flex flex-col border-[.2rem] border-primary-custom">
                         <h1 className=" absolute -top-5 lg:-top-9 left-16 p-2 bg-white font-semibold text-xl lg:text-5xl text-primary-custom">
-                            New Comments <i class="fas fa-yin-yang"></i>
+                            New Comments 
                         </h1>
                         <form
                             onSubmit={SumbitComment}
